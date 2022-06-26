c51b643752d9f64afc729c47f436b59f71f66898
TDsungw <guangwei.sun@china.talkdesk.com>
2022-06-26 15:14:46 +0800

    rabbitmq

---
+++ rabbitmq/src/main/java/com/sun/Application.java
@@ -0,0 +1,16 @@
+package com.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sungw
+ * @version 1.0
+ * @date 2021/8/5 2:20 下午
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ rabbitmq/src/main/java/com/sun/three/Task02.java
@@ -0,0 +1,32 @@
+package com.sun.three;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
+import com.sun.utils.RabbitMQutils;
+
+import java.util.Scanner;
+
+/**
+ * @author sungw
+ * @version 1.0
+ * @date 2021/8/9 3:12 下午
+ */
+public class Task02 {
+    private static final String TASK_QUEUE_NAME = "ack_queue";
+
+    public static void main(String[] argv) throws Exception {
+        try (Channel channel = RabbitMQutils.getChannel()) {
+            //消息持久化
+            boolean durable = true;
+//            channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入信息");
+            while (sc.hasNext()) {
+                String message = sc.nextLine();
+                //MessageProperties.PERSISTENT_TEXT_PLAIN发送消息为持久化消息，保存在磁盘上，
+                channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes("UTF-8"));
+                System.out.println("生产者发出消息：" + message);
+            }
+        }
+    }
+}
