a06531ad0aa334e23bb12ffa1a0ec8cca4ec1362
TDsungw <guangwei.sun@china.talkdesk.com>
2022-07-09 13:31:50 +0800

    rabbitMQ 死信队列

--- rabbitmq/src/main/java/com/sun/eight/Consumer01.java
+++ rabbitmq/src/main/java/com/sun/eight/Consumer01.java
@@ -24,16 +24,16 @@ public class Consumer01 {
         //声明死信和普通交换机 类型为 direct
         channel.exchangeDeclare(NORMAL_EXCHANGE, BuiltinExchangeType.DIRECT);
         channel.exchangeDeclare(DEAD_EXCHANGE, BuiltinExchangeType.DIRECT);
-//声明死信队列
+        //声明死信队列
         String deadQueue = "dead-queue";
         channel.queueDeclare(deadQueue, false, false, false, null);
-//死信队列绑定死信交换机与 routingkey
+        //死信队列绑定死信交换机与 routingkey
         channel.queueBind(deadQueue, DEAD_EXCHANGE, "lisi");
-//正常队列绑定死信队列信息
+        //正常队列绑定死信队列信息
         Map<String, Object> params = new HashMap<>();
-//正常队列设置死信交换机 参数 key 是固定值
+        //正常队列设置死信交换机 参数 key 是固定值
         params.put("x-dead-letter-exchange", DEAD_EXCHANGE);
-//正常队列设置死信 routing-key 参数 key 是固定值
+        //正常队列设置死信 routing-key 参数 key 是固定值
         params.put("x-dead-letter-routing-key", "lisi");
         String normalQueue = "normal-queue";
         channel.queueDeclare(normalQueue, false, false, false, params);
